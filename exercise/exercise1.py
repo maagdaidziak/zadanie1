1b0a798b9e0714e0398673e70eabef2aff2c07d0
Magda Idziak <magda99@onet.eu>
2024-03-13 11:16:07 +0100

    dodanie figur

--- exercise/exercise1.py
+++ exercise/exercise1.py
@@ -2,4 +2,9 @@ bok_a = 10
 pole_kw = bok_a*bok_a
 print("pole kwadratu wynosi: " + str(pole_kw))
 
-bok_b
+bok_b = 1
+bok_c = 4
+
+pole_pr = bok_b*bok_c
+print("pole prostokąta wynosi: " + str(pole_pr))
+
