f90e1f635fd15021ad8302e2b0e73fe62c4460e3
Magda Idziak <magda99@onet.eu>
2024-03-13 11:18:08 +0100

    dodanie koła

--- exercise/exercise1.py
+++ exercise/exercise1.py
@@ -8,3 +8,13 @@ bok_c = 4
 pole_pr = bok_b*bok_c
 print("pole prostokąta wynosi: " + str(pole_pr))
 
+prom = 4
+pi = 3.14
+pole_kl = prom**2*pi
+print("pole kola wynosi: " + str(pole_kl))
+
+
+dlu_1 = 4
+dlu_2 = 6
+pole_troj = dlu_1*dlu_2*1/2
+print("pole trojkata wynosi: " + str(pole_troj))
